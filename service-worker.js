85a32cd875129252cef3fcddc0b44e2db07aa98a
gabGodiskova <gabgodiskova@gmail.com>
2022-04-01 10:33:25 +0000

    Deploying to docs from @ books-are-next/limrova-kukacka@3439cb3a91314660f1c5652826c9e4d0a0f8e579 🚀

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-20cc9db';
+const CACHE = 'cache-3439cb3';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -40,7 +40,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./kukacka_002.html","./kukacka_005.html","./kukacka_006.html","./kukacka_007.html","./kukacka_008.html","./kukacka_009.html","./kukacka_010.html","./kukacka_011.html","./kukacka_012.html","./kukacka_013.html","./kukacka_014.html","./kukacka_015.html","./kukacka_016.html","./kukacka_017.html","./kukacka_018.html","./kukacka_019.html","./kukacka_020.html","./kukacka_021.html","./kukacka_022.html","./kukacka_023.html","./kukacka_024.html","./kukacka_025.html","./kukacka_026.html","./kukacka_027.html","./kukacka_028.html","./kukacka_029.html","./kukacka_030.html","./kukacka_031.html","./kukacka_032.html","./kukacka_033.html","./kukacka_034.html","./kukacka_035.html","./kukacka_036.html","./kukacka_037.html","./kukacka_038.html","./kukacka_039.html","./kukacka_040.html","./kukacka_041.html","./kukacka_042.html","./kukacka_043.html","./kukacka_044.html","./kukacka_045.html","./kukacka_046.html","./manifest.json","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/obalka_kukacka.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./kukacka_002.html","./kukacka_005.html","./kukacka_006.html","./kukacka_007.html","./kukacka_008.html","./kukacka_009.html","./kukacka_010.html","./kukacka_011.html","./kukacka_012.html","./kukacka_013.html","./kukacka_014.html","./kukacka_015.html","./kukacka_016.html","./kukacka_017.html","./kukacka_018.html","./kukacka_019.html","./kukacka_020.html","./kukacka_021.html","./kukacka_022.html","./kukacka_023.html","./kukacka_024.html","./kukacka_025.html","./kukacka_026.html","./kukacka_027.html","./kukacka_028.html","./kukacka_029.html","./kukacka_030.html","./kukacka_031.html","./kukacka_032.html","./kukacka_033.html","./kukacka_034.html","./kukacka_035.html","./kukacka_036.html","./kukacka_037.html","./kukacka_038.html","./kukacka_039.html","./kukacka_040.html","./kukacka_041.html","./kukacka_042.html","./kukacka_043.html","./kukacka_044.html","./kukacka_045.html","./manifest.json","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/obalka_kukacka.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
 }
 
 self.addEventListener('fetch', e => {
